d2ad6846eae8525b15d1050a76c58c326feee820
kietgula <kietgula@gmail.com>
2025-01-16 20:32:12 +0700

    thêm script enemy

---
+++ PTGame_AniForce/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100f;
+    public float damage = 10f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    protected virtual void _Attack(float damage, GameObject player)
+    {
+        
+    }
+
+    public void Attack(float damage)
+    {
+        var player = GameObject.FindGameObjectsWithTag("Player");
+        _Attack(damage, player[0]);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
